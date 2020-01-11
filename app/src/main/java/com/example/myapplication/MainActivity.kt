0f6360b4be41a76e2f6fa2425a18ba8b7e7f8dec
Joe <joe_araujo@hotmail.com>
2020-01-11 10:59:51 -0300

    Adição de comentários para melhor compreensão do funcionamento da lista

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,28 +10,39 @@ import com.example.myapplication.RecyclerView.RecyclerViewAdapter
 
 class MainActivity : AppCompatActivity() {
 
+    //A inicialização das seguintes variáveis é respectivamente ligada a manipulação da Recyclerview,
+    //manipulação do Adapter da lista e o LayoutManager necessário para a criação da lista.
+
     private lateinit var recyclerView: RecyclerView
     protected open lateinit var viewAdapter: RecyclerViewAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
-    protected open lateinit var adapter : RecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        //Aqui criamos um pequeno array de strings que será utilizado como nossa "data" passada para
+        //o adapter, afim de criar uma Row para cada item.
         var data = arrayOf<String>("Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze")
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //O LayoutManager funciona como um gerenciador de layouts da sua RecyclerView, nele é possível
+        //"settar" o formato no qual a lista será exibida assim como outras propriedades
         viewManager = LinearLayoutManager(this)
-        viewAdapter =
-            RecyclerViewAdapter(data)
 
+        //Bem como o LayoutManager também precisamos "settar" nosso Adapter, que tem como função a
+        //criação de cada Row da nossa lista
+        viewAdapter = RecyclerViewAdapter(data)
+
+        //Nessa etapa chamamos a função contida no nosso adapter para tratarmos os click's recebidos
+        //pelo listener da nossa lista.
         viewAdapter.setOnItemClickListener(object :
             ItemClickListener {
             override fun onClick(v: View?) {}
             override fun onItemClick(position: Int) {}
         })
 
+        //Aplicando algumas propriedades não necessárias para a formatação da nossa lista
         recyclerView = findViewById<RecyclerView>(R.id.recyclerList).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
--- app/src/main/java/com/example/myapplication/RecyclerView/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/myapplication/RecyclerView/RecyclerViewAdapter.kt
@@ -10,6 +10,7 @@ import com.example.myapplication.R
 
 class RecyclerViewAdapter(private val myDataset: Array<String>) : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> () {
 
+    //Inicialização da instância da interface ItemClickListener
     var itemClickListener: ItemClickListener? = null
 
     fun setOnItemClickListener(itemClickListener: ItemClickListener) {
@@ -30,17 +31,20 @@ class RecyclerViewAdapter(private val myDataset: Array<String>) : RecyclerView.A
         }
     }
 
+    //Primeira propriedade necessária para a classe Adapter, nela a view é "criada" de acordo com o
+    //espaço necessário na tela. Ela retornará uma view (o seu layout) para a classe MyViewHolder tratar
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false)
-        return MyViewHolder(
-            view
-        )
+        return MyViewHolder(view)
     }
 
+    //Segunda propriedade necessária para a classe Adapter, retorna a quantidade de elementos de uma lista
     override fun getItemCount(): Int {
         return myDataset.size
     }
 
+    //Terceira propriedade necessária para a classe Adapter, cria o vínculo entre a informação recebida
+    //pelo nosso Adapter (no caso, um array de strings) e a View especificada no XML
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.number.text = myDataset[position]
     }
